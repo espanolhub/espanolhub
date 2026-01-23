b021509b24c1562e896bd217aaffa3f6ec9cbab8
misterbuselbouti-dev <misterbus.elbouti@gmail.com>
2026-01-23 19:04:38 +0100

    Update games logic and improve footer UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P4HL68DB');</script>
+<!-- End Google Tag Manager -->
 import type { Metadata } from "next";
 import { Inter, Geist_Mono, Cairo } from "next/font/google";
 import "./globals.css";
@@ -200,3 +207,7 @@ export default function RootLayout({
     </html>
   );
 }
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4HL68DB"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
